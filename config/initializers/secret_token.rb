02ad2f595ba7938778d8a57d293129561d016c7e
Nick Gauthier <ngauthier@gmail.com>
2012-12-20 15:25:12 -0500

    initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapybaraJasmineRails::Application.config.secret_token = '18ee15b225eb0451f7eef6ebe7eee0ead27907280abd456b7852225a47c5e8eb637c0f63639d1a413ae2333a43d6a0a6b995418eb88fc787c65954ac3d3f0e51'
